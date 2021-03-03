d523edfaa83982179960dc2372c8cfcf034241f1
dudinhadnm <dudinha.dnm@gmail.com>
2021-03-03 13:34:53 -0300

    Commit of plot3: R code and png

--- plot3.R
+++ plot3.R
@@ -8,9 +8,10 @@ electricpower$Date <- as.Date(electricpower$Date, "%d/%m/%Y")
 electric <- subset(electricpower, Date == "2007-02-01"| Date == "2007-02-02")
 electric$datetime <- strptime(paste(electric$Date, electric$Time, sep =" "), format = "%Y-%m-%d%H:%M:%S")
 
-
+png("plot3.png")
 with(electric, plot(datetime, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering"))
 lines(electric$datetime, electric$Sub_metering_1, col = "black")
 lines(electric$datetime, electric$Sub_metering_2, col = "red")
 lines(electric$datetime, electric$Sub_metering_3, col = "blue")
 legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
